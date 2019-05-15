75fce4e574588764c35b67f25bbc72cd3be3ff69
Lxj <lai8206@126.com>
2019-05-15 22:59:51 +0800

    添加bower注册信息

---
+++ README.md
@@ -0,0 +1,6 @@
+# bower 打包发布
+
+```bash
+yarn add global bower-nexus3-resolver
+bower register jquery.pjax http://git.zx/product/zx-component/jquery-pjax.git
+```
---
+++ src/main/java/com/zx/webjars/App.java
@@ -0,0 +1,11 @@
+package com.zx.webjars;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
